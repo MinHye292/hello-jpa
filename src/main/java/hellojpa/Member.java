dc49f1a9c74662c64f1cfe466027c92b2f4a694d
minhye292 <8547364@naver.com>
2024-01-29 22:37:50 +0900

    Hello JPA - 애플리케이션 개발

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,27 @@
+package hellojpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+    @jakarta.persistence.Id
+    private Long Id;
+    private String name;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
