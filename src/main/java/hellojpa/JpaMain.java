03c0b25a142d6181de483406b78df940e6054df0
minhye292 <8547364@naver.com>
2024-03-15 20:21:34 +0900

    고아객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,6 +21,12 @@ public class JpaMain {
             parent.addChild(child2);
 
             em.persist(parent);
+
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
             
             tx.commit();
         } catch (Exception e) {
